2808ecb6270477e003dee440a77edc9b3f7742c4
Sanskar Pandey <142474055+sanskarpan@users.noreply.github.com>
2026-05-21 20:17:56 +0530

    api: allow replication header in CORS (#277)

--- api/middleware.go
+++ api/middleware.go
@@ -50,7 +50,7 @@ func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Tenant-ID")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Tenant-ID, X-Trace-Replication")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(204)
 			return
